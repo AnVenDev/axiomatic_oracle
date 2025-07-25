91cc724fb74ba9833ed8ad5d635d1f4b8c9ac83b
Andrea Venè <anvene.dev@gmail.com>
2025-07-25 13:50:10 +0200

    Fixed secrets_manager structure

--- scripts/secrets_manager.py
+++ scripts/secrets_manager.py
@@ -1,14 +1,18 @@
 import os
-
 from dotenv import load_dotenv
 
 IS_CI = os.getenv("CI", "false").lower() == "true"
 
+# Network URLs
+ALGO_NETWORKS = {
+    "testnet": "https://testnet-api.algonode.cloud",
+    "mainnet": "https://mainnet-api.algonode.cloud",
+}
+
 if IS_CI:
     ALGORAND_WALLET_ADDRESS = "FAKE_WALLET"
     ALGORAND_MNEMONIC = "FAKE_MNEMONIC"
-    ALGORAND_NETWORK = "FAKE_NETWORK"
-    ALGORAND_ADDRESS = "FAKE ADDRESS"
+    ALGORAND_NETWORK = "testnet"  # valore valido per evitare errori
 else:
     load_dotenv()
     ALGORAND_WALLET_ADDRESS = os.getenv("ALGORAND_WALLET_ADDRESS")
@@ -18,10 +22,5 @@ else:
 if not ALGORAND_WALLET_ADDRESS or not ALGORAND_MNEMONIC:
     raise ValueError("Algorand credentials not properly set.")
 
-# Network URLs
-ALGO_NETWORKS = {
-    "testnet": "https://testnet-api.algonode.cloud",
-    "mainnet": "https://mainnet-api.algonode.cloud",
-}
+# Valid ALGORAND_ADDRESS based on network
 ALGORAND_ADDRESS = ALGO_NETWORKS.get(ALGORAND_NETWORK)
-
