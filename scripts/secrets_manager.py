f94cb18735ae9b7ea0f8baa3021ecaa1fb5a5321
Andrea Venè <anvene.dev@gmail.com>
2025-07-25 13:39:06 +0200

    CI check in secrets_manager

--- scripts/secrets_manager.py
+++ scripts/secrets_manager.py
@@ -2,13 +2,17 @@ import os
 
 from dotenv import load_dotenv
 
-# Load .env file
-load_dotenv()
+IS_CI = os.getenv("CI", "false").lower() == "true"
 
-# Load secrets
-ALGORAND_WALLET_ADDRESS = os.getenv("ALGORAND_WALLET_ADDRESS")
-ALGORAND_MNEMONIC = os.getenv("ALGORAND_MNEMONIC")
-ALGORAND_NETWORK = os.getenv("ALGORAND_NETWORK", "testnet")
+if IS_CI:
+    ALGORAND_ADDRESS = "FAKE_ADDRESS"
+    ALGORAND_MNEMONIC = "FAKE_MNEMONIC"
+    ALGORAND_WALLET_ADDRESS = "FAKE_WALLET"
+else:
+    load_dotenv()
+    ALGORAND_ADDRESS = os.getenv("ALGORAND_ADDRESS")
+    ALGORAND_MNEMONIC = os.getenv("ALGORAND_MNEMONIC")
+    ALGORAND_WALLET_ADDRESS = os.getenv("ALGORAND_WALLET_ADDRESS")
 
 if not ALGORAND_WALLET_ADDRESS or not ALGORAND_MNEMONIC:
     raise ValueError("Algorand credentials not properly set.")
